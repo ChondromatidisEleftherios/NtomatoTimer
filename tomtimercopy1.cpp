effd00a81f2eabebf68e80f338d880967ebf8d4d
ChondromatidisEleftherios <148573871+ChondromatidisEleftherios@users.noreply.github.com>
2023-11-18 10:55:33 +0200

    Update tomtimercopy1.cpp

--- tomtimercopy1.cpp
+++ tomtimercopy1.cpp
@@ -70,41 +70,41 @@ int main()
 {
     pomodoro (); //κλήση default constructor//
     pomodoro me;
-     const string filename = "cs.txt";
-    const string filename1 = "twt.txt";
+     const string filename = "cs.txt"; //ορισμός ονόματος αρχείου για σύνολο ολοκληρωμένων συνεδριών//
+    const string filename1 = "twt.txt"; //oρισμός ονόματος αρχείου για συνολικό χρόνο εργασίας//
     int re=1, pressedset=0, cho=0, piswstomenu=0;
     char ans;
             ifstream file(filename);
-        if (file.is_open()) {
+        if (file.is_open()) {          //Έλεγχος εαν το αρχείο cs.txt όντως υπαρχει//
                 cout << " ";
         }
         else
         {
-           ofstream filename;
+           ofstream filename; // Aν το αρχείο ΔΕΝ υπάρχει, θα φτιαχτεί νέο αρχείο//
            filename.open ("cs.txt");
-           filename<< 0;
+           filename<< 0; //Αρχικοποίηση αρχείου με 0//
           filename.close();
         }
          ifstream file1(filename1);
-        if (file1.is_open()) {
+        if (file1.is_open()) {    //Έλεγχος αν το αρχείο twt.txt όντως υπάρχει//
                 cout << " ";
         }
         else
         {
-           ofstream filename1;
+           ofstream filename1; //Aν το αρχείο ΔΕΝ υπάρχει, θα φτιαχτεί νέο αρχείο twt.txt//
            filename1.open ("twt.txt");
-           filename1<< 0;
+           filename1<< 0;  //Αρχικοποίηση αρχείου με 0//
           filename1.close();
         }
 
     while (re!=2)
     {
    HANDLE col= GetStdHandle(STD_OUTPUT_HANDLE);
-   SetConsoleTextAttribute(col,7);
+   SetConsoleTextAttribute(col,7); //Αλλαγή χρώματος κονσόλας//
     cho=0;
     piswstomenu=0;
-    cho=menu();
-    if (cho==0)
+    cho=menu(); //Κλήση συνάρτησης menu για εμφάνιση του μενού επιλογών//
+    if (cho==0)  //Έξοδος από την εφαρμογή αν η menu επέστρεψε τιμή 0//
     {
         re=2;
         system("cls");
@@ -118,12 +118,12 @@ int main()
         _sleep(500);
        return 0;
     }
-    else if (cho==1)
+    else if (cho==1) // Αν η menu επέστρεψε τιμή 1//
     {
-        if (pressedset==0)
+        if (pressedset==0) //Αν ο χρήστης δεν έχει πάει στα settings για να αλλάξει τους χρόνους//
         {
-            me.setworkduration(25);
-            me.setbreakduration(5);
+            me.setworkduration(25); //Χρόνος εργασίας τα 25 λεπτά//
+            me.setbreakduration(5); //Χρόνος διαλείμματος τα 5 λεπτά//
         }
             piswstomenu=me.startSession();
         if (piswstomenu!=69)
@@ -133,7 +133,7 @@ int main()
                     SetConsoleTextAttribute(col,7);
                     piswstomenu==0;
             cout << "Press 1 To Continue With The Same Settings" << endl;
-            ans=getch();
+            ans=getch(); //Διάβασμα τιμής από τον χρήστη για το αν επιθυμεί να συνεχίζει με τις ίδιες ρυθμίσεις//
                  if((ch==0)&&(ans=='1'))
 
                  {
